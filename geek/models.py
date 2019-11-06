4d930307f9f49c8a4d41fd889801d9080d62c84b
agnes <aggyreina@gmail.com>
2019-11-06 15:50:09 +0200

    creates model

--- geek/models.py
+++ geek/models.py
@@ -1,3 +1,52 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Programmers_profile(models.Model):
+    names = models.CharField(max_length =30)
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    profile_photo = models.ImageField(upload_to = 'profile_photos/', null=True)
+    github_link = models.CharField(max_length =30)
+    date = models.DateTimeField(auto_now_add=True)
+    degree= models.CharField(max_length =30)
+    @classmethod
+    def get_all_programmers(cls):
+        instagram_users = cls.objects.all()
+        return instagram_users
+    def save_programmer(self):
+        self.save()
+    def delete_programmer(self):
+        self.delete()
+    @classmethod
+    def update_programmer(cls,id,value):
+        cls.objects.filter(id = id).update(user_id = new_user)
+    @classmethod
+    def search_by_programmer(cls,names):
+        certain_user = cls.objects.filter(user__names__icontains = names)
+        return certain_user
+    def __str__(self):
+        return self.user
+
+class Project(models.Model):
+    title = models.CharField(max_length =30)
+    project_image = models.ImageField(upload_to = 'project_images/', null=True)
+    description = models.CharField(max_length =300)
+    link = models.URLField(max_length=128, db_index=True, unique=True, null=True)
+    posred_date = models.DateTimeField(auto_now_add=True, null=True)
+    user = models.ForeignKey(User,on_delete=models.CASCADE, null=True)
+    programmers = models.ForeignKey(Programmers_profile,on_delete=models.CASCADE, null=True)
+
+    @classmethod
+    def get_all_projects(cls):
+        all_projects = cls.objects.all()
+        return all_projects
+
+    def save_projects(self):
+        self.save()
+
+    def delete_projects(self):
+        self.delete()
+        
+    def __str__(self):
+        return self.title
--- geek/views.py
+++ geek/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from django.http  import HttpResponse,HttpResponseRedirect
+from .models import Programmers_profile,Project
+from django.contrib.auth.decorators import login_required
+from .forms import NewProfileForm
 # Create your views here.
+
+def welcome(request):
+    current_user = request.user
+    profiless = Profile.objects.filter(id = current_user.id).first()
+    all_projects = Project.get_all_projects()
+    return render(request, 'welcome.html', {"all_projects": all_projects,"profiless":profiless})
+
+@login_required(login_url='/accounts/login/')
+def upload_project(request):
+    current_user = request.user
+    if request.method == 'POST':
+        form = NewProjectForm(request.POST, request.FILES)
+        if form.is_valid():
+            image = form.save(commit=False)
+            image.user = current_user
+            image.save()
+        return redirect('index')
+
+    else:
+        form = NewProjectForm()
+    return render(request, 'upload_project.html', {"form": form})
+
