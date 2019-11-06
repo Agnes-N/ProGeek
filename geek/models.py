45b7ad0426409a81a42f42fa87dfc87194e50a7b
ClarisseU <clarisseum@gmail.com>
2019-11-06 15:57:06 +0200

    add chat model

--- geek/models.py
+++ geek/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Chat(models.Model):
+    chat_content = models.CharField(max_length=120)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    
+    def save_chat(self):
+        self.save()
+        
+    def delete_chat(self):
+        self.delete()    
