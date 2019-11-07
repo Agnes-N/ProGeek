81cbcda5a9a4f6c2ec3bb97dbdf6aa84f398e029
agnes <aggyreina@gmail.com>
2019-11-07 11:10:34 +0200

    added templates

--- geek/admin.py
+++ geek/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Programmers_profile,Project,Chat
 
 # Register your models here.
+admin.site.register(Project)
+admin.site.register(Programmers_profile)
+admin.site.register(Chat)
